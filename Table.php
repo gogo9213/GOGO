e96c765d74c69672407ced7b7ad19036f7514713
gogo9213 <gogo9213@abv.bg>
2015-03-09 19:47:34 +0200

    Update Table.php

--- Table.php
+++ Table.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html>
-<head lang="eng">
-	<title>Unnamed</title>
-	<meta charset="UTF-8" />
-</head>
-<body>
-<form action= "<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
-Въведете редове:<input type="text" name="rows" size="10" /><br />
-Въведете колони:<input type="text" name="cells" size="10" /><br />
-				<input type="submit" name="send" value="OK" />
-				</form>
-				<?php	
-					if(isset ($_SERVER['send'])){
-						$rows=$_GET['rows'];
-						$cells=$_GET['cells'];
-							if(is_numeric($rows) && is_numeric($cells)){
-								echo '<table border="1">';
-								for($i=0;$i<=$_GET['rows'];$i++){
-									echo '<tr>';
-									for($j=0;$j<=$_GET['cells'];$j++){
-										echo '<td>'.$cells.'</td>';
-									}
-									echo '</tr>';
-								}
-								echo '</table>';
+	<head lang="eng">
+		<title>Unnamed</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<form action= "<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+			Въведете редове:<input type="text" name="rows" size="10" /><br />
+			Въведете колони:<input type="text" name="cells" size="10" /><br />
+						    <input type="submit" name="send" value="OK" /><br />
+		</form>
+			<?php	
+				if(isset ($_SERVER['send'])){
+					$rows=$_GET['rows'];
+					$cells=$_GET['cells'];
+					if(is_numeric($rows) && is_numeric($cells)){
+						echo '<table border="1">';
+						for($i=1;$i<=$rows;$i++){
+							echo '<tr>';
+							for($j=1;$j<=$cells;$j++){
+								echo '<td>'.$cells.'</td>';
 							}
+							echo '</tr>';
+						}
+						echo '</table>';
 					}
-				?>
-</body>
-</html>
+					else{
+						echo  "Некоректно въведени стойности, моля въведете числена стойност в полетата!";
+					}
+				}
+			?>
+	</bo
